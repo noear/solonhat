8fd2154b6e53c4f8b4ee5fb0c1dab66e16359233
hanxiao <xiao.han@smartriver.com.cn>
2021-12-08 12:44:09 +0800

    solon smartdoc adapter  init

---
+++ solonhat-smartdoc/src/main/java/org/noear/solonhat/smartdoc/Constants.java
@@ -0,0 +1,20 @@
+package org.noear.solonhat.smartdoc;
+
+public class Constants {
+
+
+    public static  final String   REQUEST_MAPPING ="Mapping";
+    public static  final String   REQUEST_MAPPING_FULLY ="org.noear.solon.annotation.Mapping";
+
+    public static final String GET_MAPPING = "Get";
+    public static final String GET_MAPPING_FULLY ="org.noear.solon.annotation.Get" ;
+
+    public static final String POST_MAPPING ="Post" ;
+    public static final String POST_MAPPING_FULLY = "org.noear.solon.annotation.Post";
+    public static final String PUT_MAPPING = "Put";
+    public static final String PUT_MAPPING_FULLY = "org.noear.solon.annotation.Put";
+    public static final String PATCH_MAPPING = "Patch";
+    public static final String PATCH_MAPPING_FULLY = "org.noear.solon.annotation.Patch";
+    public static final String DELETE_MAPPING = "Delete";
+    public static final String DELETE_MAPPING_FULLY = "org.noear.solon.annotation.Delete";
+}
