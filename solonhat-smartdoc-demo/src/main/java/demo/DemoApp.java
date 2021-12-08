795acf87020ce1ac05a5bd2dd7758d108c64a4ec
noear <noear@live.cn>
2021-12-08 22:53:03 +0800

    1.0.0

--- solonhat-smartdoc-demo/src/main/java/demo/DemoApp.java
+++ solonhat-smartdoc-demo/src/main/java/demo/DemoApp.java
@@ -5,7 +5,7 @@ import org.noear.solon.Solon;
 public class DemoApp {
     public static void main(String[] args) {
         Solon.start(DemoApp.class, args).get("/", c -> {
-            c.redirect("/doc/api.htm");
+            c.redirect("/doc/api.html");
         });
     }
 }
