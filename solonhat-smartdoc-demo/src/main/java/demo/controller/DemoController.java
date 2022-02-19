74805a71625c96c4e0cb0aaf297a9f1ad9ce1c90
noear <noear@live.cn>
2022-02-20 00:17:19 +0800

    1.0.0-m1

--- solonhat-smartdoc-demo/src/main/java/demo/controller/DemoController.java
+++ solonhat-smartdoc-demo/src/main/java/demo/controller/DemoController.java
@@ -15,20 +15,21 @@ import java.util.Map;
  */
 @Controller
 @Mapping("/demo")
-public class DemoController{
+public class DemoController {
 
     /**
      * hello
+     *
      * @param username 用户
      * @return
      */
     @Get
     @Mapping("/hello/{username}")
-    public Result<Msg> hello(String username){
-        Map m=new HashMap();
+    public Result<Msg> hello(String username) {
+        Map m = new HashMap();
 
-        m.put("uu",username);
-        Msg msg=new Msg();
+        m.put("uu", username);
+        Msg msg = new Msg();
         msg.setData(m);
 
         return Result.succeed(msg);
@@ -36,56 +37,60 @@ public class DemoController{
 
     /**
      * addapp
+     *
      * @param appname app名称
-     * @param nick 昵称
+     * @param nick    昵称
      * @return
      */
     @Post
-    @Mapping("/add")
-    public Msg add(String appname,String nick){
-        Map m=new HashMap();
-        m.put("appname",appname);
-        m.put("nick",nick);
+    @Mapping(value = "/add", produces = "text/json")
+    public Msg add(String appname, String nick) {
+        Map m = new HashMap();
+        m.put("appname", appname);
+        m.put("nick", nick);
 
-        Msg msg=new Msg();
+        Msg msg = new Msg();
         msg.setData(m);
         return msg;
     }
 
     /**
      * add by model
+     *
      * @param model
      * @return
      */
     @Post
-    @Mapping("/add")
-    public Msg add(Model model){
-        Msg m=new Msg();
+    @Mapping(value = "/add", produces = "text/json")
+    public Msg add(Model model) {
+        Msg m = new Msg();
         return m;
     }
 
     /**
      * update app
-     * @param id id
+     *
+     * @param id      id
      * @param appname app名称
-     * @param nick 昵称
+     * @param nick    昵称
      * @return
      */
     @Put
     @Mapping("/update")
-    public Msg update(Long id,String appname,String nick){
-        Msg m=new Msg();
+    public Msg update(Long id, String appname, String nick) {
+        Msg m = new Msg();
         return m;
     }
 
     /**
      * get by appname
+     *
      * @param appname
      * @return
      */
     @Mapping("/get/{appname}")
-    public Msg get(String appname){
-        Msg m=new Msg();
+    public Msg get(String appname) {
+        Msg m = new Msg();
         return m;
     }
 
